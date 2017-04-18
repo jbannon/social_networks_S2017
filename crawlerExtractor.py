2e9e91c5a9f8f10723fe2055dcb020b76f12add2
nishanthpp93 <nishanthpp93@users.noreply.github.com>
2017-04-18 15:31:27 -0400

    Updated crawlerExtractor

--- crawlerExtractor.py
+++ crawlerExtractor.py
@@ -7,19 +7,24 @@ from timeout import timeout
 
 hdr = { 'User-Agent' : 'super friendly bot' }
 MIN_ARTICLE_LENGTH = 500
-MAX_ARTICLES = 50
+MAX_ARTICLES = 10
 
 def getRelatedURLs(url):
 	relatedURLs = []
 	# try:
-	# 	response = requests.get(url)
+	# 	response = requests.get(url, headers=hdr)
 	# 	html = response.content
 	# except:
-	opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
-	req = urllib2.Request(url, headers=hdr)
-	html = opener.open(req)
-	for link in BeautifulSoup(html, parse_only=SoupStrainer("a", href=True)).find_all("a"):
-		relatedURLs.append(link['href'])
+	try:
+		opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
+		req = urllib2.Request(url, headers=hdr)
+		html = opener.open(req)
+		for link in BeautifulSoup(html, parse_only=SoupStrainer("a", href=True)).find_all("a"):
+			relatedURL = link['href'].split('#')[0]
+			relatedURLs.append(relatedURL)
+	except:
+		pass
+	relatedURLs = list(set(relatedURLs))
 	return relatedURLs
 
 @timeout(10)
@@ -31,7 +36,7 @@ def getContentFromNewspaper(url):
 	content = article.text
 	title = article.title
 	authors = article.authors
-	date = article.publish_date
+	date = str(article.publish_date)
 	return content, title, authors, date
 
 @timeout(10)
@@ -44,7 +49,7 @@ def getContentFromBoilerpipe(url):
 def getContent(url):
 	content, title, authors, date = (u'', )*4
 	try:
-		content, authors, title = getContentFromNewspaper(url)
+		content, authors, title, date = getContentFromNewspaper(url)
 	except:
 		try:
 			content = getContentFromBoilerpipe(url)
@@ -53,42 +58,50 @@ def getContent(url):
 	return content, title, authors, date
 
 def isPotentialArticle(url):
-	if (url[-1] != "/") and (url.count('-') > 4) \
-						and not any(exclude in url for exclude in nonArticles):
+	if (url.count('-') > 3 or url.count('+') > 3 or url.count('_') > 3 or "article" in url or "content" in url) \
+			and not any(exclude in url for exclude in nonArticles):
 		return True
 	else:
 		return False
 
-listlinks = []
-main = "http://www.npr.org"
-toVisit = [main]
-toVisitandVisited = toVisit
-nonArticles = ["video", "facebook", "twitter", "digg", "pinterest", "mailto", "linkedin", "#"]
-relatedURLs = []
+nonArticles = ["video", "picture", "image", "facebook", "twitter", "digg", "pinterest", "mailto", "linkedin", "google"]
 
-with io.open('articles.txt', 'w', encoding="utf-8") as output:
-	if (main[-1] == "/"):	# for concatenation of sublinks which start with "/"
-		main = main[:-1]
-	while(len(listlinks) < MAX_ARTICLES and len(toVisit) > 0):
-		url = toVisit[0]
-		toVisit = toVisit[1:]
-		content, title, authors, date = getContent(url)
+with open('inputSites.txt', 'r') as input, io.open('articles.txt', 'w', encoding="utf-8") as output:
+	for main in input:
+		main = main.strip()
+		if (main[-1] == "/"):	# for concatenation of sublinks which start with "/"
+			main = main[:-1]
+		toVisit = [main]
+		toVisitandVisited = toVisit
+		listlinks = []
+		relatedURLs = []
+		while(len(listlinks) < MAX_ARTICLES and len(toVisit) > 0):
+			url = toVisit[0]
+			toVisit = toVisit[1:]
+			content, title, authors, date = getContent(url)
 
-		if (url == main) or (url != main and len(content) > MIN_ARTICLE_LENGTH):
-			if len(content) > MIN_ARTICLE_LENGTH:
-				data = {"url" : url, "title" : title, "date" : date, "authors" : authors, "content" : content}
-				output.write(json.dumps(data, ensure_ascii=False))
-				output.write(u'\n')
-				listlinks.append(url)
+			if (url == main) or (url != main and len(content) > MIN_ARTICLE_LENGTH):
+				if len(content) > MIN_ARTICLE_LENGTH:
+					data = {"main": main, "url" : url, "title" : title, "date" : date, "authors" : authors, "content" : content}
+					output.write(json.dumps(data, ensure_ascii=False))
+					output.write(u'\n')
+					listlinks.append(url)
+				
+				relatedURLs = getRelatedURLs(url)
+				for relatedURL in relatedURLs:
+					if (isPotentialArticle(relatedURL)):
+						if relatedURL[0] == "/":
+							relatedURL = main + relatedURL
+						elif relatedURL[0] != "h":
+							relatedURL = main + "/" + relatedURL
+						if main[5:] in relatedURL:						# if https links fetched but main is http
+							if relatedURL not in toVisitandVisited:
+								toVisit.append(relatedURL)
+								toVisitandVisited.append(relatedURL)
+		listlinks = []
+		toVisit = []
+		relatedURLs = []
+		toVisitandVisited = []
 
-			relatedURLs = getRelatedURLs(url)
-			for relatedURL in relatedURLs:
-				if (isPotentialArticle(relatedURL)):
-					if relatedURL[0] == "/":
-						relatedURL = main + relatedURL
-					if main[5:] in relatedURL:						# if https links fetched but main is http
-						if relatedURL not in toVisitandVisited:
-							toVisit.append(relatedURL)
-							toVisitandVisited.append(relatedURL)
 
 
